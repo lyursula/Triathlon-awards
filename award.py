a5e3f49f0f62da47e364c0dbfad04fc24bffa53d
lyursula <lyanaursula@yahoo.com>
2024-03-08 21:23:55 +0000

    added threshold to improve readability of code

--- award.py
+++ award.py
@@ -4,6 +4,12 @@
 # input time for running
 # calculate total time for all three
 # workout which award to give
+
+# Define award thresholds
+PROVINCIAL_COLOURS_THRESHOLD = 100
+PROVINCIAL_HALF_COLOURS_THRESHOLD = 105
+PROVINCIAL_SCROLL_THRESHOLD = 110
+
 swimming_time = float(input("Please enter your recorded time for swimming(minutes): "))
 cycling_time = float(input("Please enter your recorded time for cycling(minutes): "))
 running_time = float(input("Please enter your recorded time for running(minutes): "))
@@ -13,11 +19,12 @@ print(f"Your total time for completing the triathlon is {total_time} minutes.")
 
 if total_time==0:
     print("You haven't participated.")
-elif (total_time >0) and (total_time <= 100):
+elif 0 < total_time <= PROVINCIAL_COLOURS_THRESHOLD:
     print("Congratulations! You've received Provincial Colours!")
-elif (total_time >=101) and (total_time <=105):
+elif PROVINCIAL_COLOURS_THRESHOLD < total_time <= PROVINCIAL_HALF_COLOURS_THRESHOLD:
     print("Congratulations! You've received Provincial Half Colours!")
-elif (total_time >=106) and (total_time <=110):
+elif PROVINCIAL_HALF_COLOURS_THRESHOLD < total_time <= PROVINCIAL_SCROLL_THRESHOLD:
     print("Congratulations! You've received Provincial Scroll!")
 else:
     print("No award.")
+    
